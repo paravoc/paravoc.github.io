fbfdcfc87cefe0a1421b4cffcd3f3a3490a457e7
paravoc <danoda2004@gmail.com>
2026-04-09 17:14:12 +0300

    Add FIPI-aligned OGE practice content

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'oge-atlas-cache-v7';
+const CACHE_NAME = 'oge-atlas-cache-v8';
 const ASSETS = [
   './',
   './index.html',
@@ -6,6 +6,7 @@ const ASSETS = [
   './content.js',
   './content-plus.js',
   './geography-plus.js',
+  './fipi-practice.js',
   './study-app.js',
   './manifest.webmanifest',
   './icon.svg'
